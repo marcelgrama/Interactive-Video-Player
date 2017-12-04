c300aa98d0bd6b1623b760805eedeed2208a3bd1
Marcel Grama <aace375@yahoo.ro>
2017-12-04 20:47:34 +0200

    p6

---
+++ js/main.js
@@ -0,0 +1,42 @@
+$('video').mediaelementplayer({
+    features: ['playpause', 'current', 'progress', 'volume', 'fullscreen'],
+    
+});
+
+const video = document.querySelector('video');
+const spans = document.querySelectorAll('.transcript span');
+
+
+video.addEventListener('timeupdate', () =>{
+
+const cTime = video.getCurrentTime();
+
+for(let i=0 ; i< spans.length ; i+=1){
+	const span = spans[i];
+	const start = parseFloat(span.dataset.start);
+	const end = parseFloat(span.dataset.end);
+	if(cTime>start && cTime<end){
+		span.classList.add("highlight");
+	}
+	else{
+		span.classList.remove("highlight");
+	}
+}
+
+});
+
+
+for(let i=0 ; i< spans.length ; i+=1){
+	const span = spans[i];
+	const start = parseFloat(span.dataset.start);
+	const end = parseFloat(span.dataset.end);
+span.addEventListener('click', () =>{
+
+if(span){
+ video.setCurrentTime(start);
+ video.play();
+}
+
+});
+
+}
